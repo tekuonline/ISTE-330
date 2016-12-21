a25e315bd0ec1c4b85d0e3635409fa7753c9aaf6
Tek Nepal <TN2089@rit.edu>
2016-12-21 11:18:10 -0500

    Update Authenticate.java

--- src/Authenticate.java
+++ src/Authenticate.java
@@ -15,7 +15,7 @@
 public interface Authenticate {
 	static final String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DATABASE_URL = "jdbc:mysql://teknepal.com:3306/ISTE330PAPER";
-	static final String user = "teku";
+	static final String user = "";
 	static final String pass = "Test123";
 
 	public abstract boolean authenticateUser(String user, String password);
